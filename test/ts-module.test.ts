abbe894159dd9a056f00b3a484625869db9ab36f
John Wright <jrite@protonmail.com>
2024-08-28 19:45:12 +0100

    test: use full module reference

--- test/ts-module.test.ts
+++ test/ts-module.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { tsModule } from '../src/index.js'
+import { tsModule } from '@johngeorgewright/ts-module/foo'
 
 test("it's a module", () => {
   expect(tsModule()).toBe('I am a TypeScript module')
