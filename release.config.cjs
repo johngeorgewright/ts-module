f354b10810309abfef5e93c55f7849cc2ffaf0d9
John Wright <jrite@protonmail.com>
2025-06-10 16:35:40 +0100

    ci: publish only certain files

--- release.config.cjs
+++ release.config.cjs
@@ -3,7 +3,7 @@
 const bumpVersion =
   'cat deno.json | jq \'.version |= "${nextRelease.version}"\' > deno.tmp.json && mv deno.tmp.json deno.json'
 const isPublicPackage = '[ $(cat deno.json | jq -r .private) != "true" ]'
-const publish = 'deno publish'
+const publish = 'deno publish --allow-dirty'
 
 /**
  * @type {import('npm:semantic-release').Options}
@@ -17,7 +17,8 @@ module.exports = {
       '@semantic-release/exec',
       {
         prepareCmd: `${isPublicPackage} && ${bumpVersion}`,
-        verifyReleaseCmd: `${isPublicPackage} && ${publish} --dry-run`,
+        verifyReleaseCmd:
+          `${isPublicPackage} && ${publish} --set-version "\${nextRelease.version}" --dry-run`,
         publishCmd: `${isPublicPackage} && ${publish}`,
       },
     ],
